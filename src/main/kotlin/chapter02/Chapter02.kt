f8bc002aab2352bf40f21e1196b7034560b537d9
최홍희 <vvshinevv@gmail.com>
2023-01-04 01:58:14 +0900

    코틀린 인 액션 2장 따라치기

--- src/main/kotlin/chapter02/Chapter02.kt
+++ src/main/kotlin/chapter02/Chapter02.kt
@@ -13,4 +13,19 @@ fun main(args: Array<String>) {
     for ((letter, binary) in binaryReps) {
         println("$letter = $binary")
     }
+
+    val list = arrayListOf("10", "11", "1001")
+    for ((i, e) in list.withIndex()) {
+        println("$i: $e")
+    }
+
+    ex1(1111)
+}
+
+fun ex1(percentage: Int) {
+    if (percentage !in 0..100) {
+        throw IllegalArgumentException("not in")
+    }
+
+    println("$percentage is 0 in 100")
 }
